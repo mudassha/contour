82604a9043372409f014fc60c85a1b667d864fcd
mudassha <mudassha@CRIBL-MUDASSHA1.tl.pvt>
2021-09-08 12:19:27 +0500

    Second Commit
    Sum of a and b added

--- Hello World/src/Main.java
+++ Hello World/src/Main.java
@@ -3,8 +3,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Hello World");
-		System.out.print("Hello World 2");
+		System.out.println("Hello World");
+		
+		
+		int a = 1;
+		int b = 2;
+		int sum = a+b;
+		System.out.println(sum);
 	}
 
 }
